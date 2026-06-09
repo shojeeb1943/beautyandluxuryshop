47b9605b15b4e24239c8582dbe3b2d25bf6329a9
Md Reyajul Islam <shojeeb1943@gmail.com>
2026-06-09 18:41:24 +0600

    fix: use predefined color palette and refine color handler in Quill editors
    Replaces the default color picker with a fixed palette and updates
    
    the color handler to apply formatting to selected text ranges.

--- public/assets/back-end/libs/quill-editor/business-pages-quill-editor-init.js
+++ public/assets/back-end/libs/quill-editor/business-pages-quill-editor-init.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
                         [{ 'align': [] }],
                         [{ 'font': [] }],
                         [{ 'size': ['small', false, 'large', 'huge'] }],
-                        [{ 'color': [] }, { 'background': [] }],
+                        [{ 'color': ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466'] }, { 'background': [] }],
                         ['blockquote', 'code-block'],
                         ['link', 'image', 'formula'],
                         [{ 'script': 'sub' }, { 'script': 'super' }],
@@ -25,12 +25,15 @@ $(document).ready(function () {
                     handlers: {
                         color: function (value) {
                             const range = this.quill.getSelection(true);
-                            if (!value || value === '#000000' || value === 'rgb(0, 0, 0)') {
+                            if (!value) {
                                 this.quill.format('color', false, 'user');
                                 if (range && range.length > 0) {
                                     this.quill.formatText(range.index, range.length, 'color', false, 'user');
                                 }
                             } else {
+                                if (range && range.length > 0) {
+                                    this.quill.formatText(range.index, range.length, 'color', value, 'user');
+                                }
                                 this.quill.format('color', value, 'user');
                             }
                         },
